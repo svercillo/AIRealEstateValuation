39cc8957865cc23fefe52ba1cafdb3c37b15065f
Stefan Vercillo <svercillo7@gmail.com>
2020-10-05 01:29:49 -0400

    Update dataclass.py

--- AIModel/dataclass.py
+++ AIModel/dataclass.py
@@ -2,7 +2,7 @@ class Object:
   def __init__(self, sp, address, bedNum, bathNum, sqft, tp, style, size, lotSize, amenities, schools):
     self.sold_price = sp
     self.address = address
-    self.bedNum = bedNum
+    self.bedNum = bedNum 
     self.bathNum = bathNum 
     self.sqft = sqft 
     self.typeOfProperty = tp
