52f82285bdb753bcdc19765423ebffa817e325cc
Stefan Vercillo <svercillo7@gmail.com>
2020-10-05 01:30:45 -0400

    Update startvenv.bat

--- AIModel/startvenv.bat
+++ AIModel/startvenv.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo off 
 
 rem This file is UTF-8 encoded, so we need to update the current code page while executing it
 for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
