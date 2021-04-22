8917e9ce3b36e4024f36050a766081c85fed4800
Stefan Vercillo <svercillo7@gmail.com>
2021-04-21 23:44:52 -0400

    csv data yayy

--- Data/concat_to_csv.py
+++ Data/concat_to_csv.py
@@ -1,33 +1,44 @@
 import json
+import csv
 
 
-class DynamoModel:
 
-	def __init__(self):
-		db = boto3.resource('dynamodb')
-        self.table = db.Table('AIPropertyData')
+# class DynamoModel:
 
-    def insert_item(self, item):
-        
-        
-		self.table.put_item(
-		Item ={'address': item['address'],
-			'longitude' : item['longitude'],
-			'latitude' : item['latitude'],
-			"data" : item
-		})
+#     def __init__(self):
+#         db = boto3.resource('dynamodb')
+#         self.table = db.Table('AIPropertyData')
+
+#     def insert_item(self, item):
 
 
-db = DynamoModel()
+#         self.table.put_item(
+#             Item ={'address': item['address'],
+#                 'longitude' : item['longitude'],
+#                 'latitude' : item['latitude'],
+#                 "data" : item
+#             })  
+
+
+# db = DynamoModel()
+
 lines = "" 
 
-for i in range(1, 2):
+rows = []
+for i in range(1, 450):
     try:
-        f = open(f"data{i}.txt", "r", encoding="utf8")
-        lines  += f.read()
+        with open(f"data{i}.txt", "r", encoding="utf8") as f:
+            flines = f.readlines()
+            for line in flines:
+                lines += line
+                data = json.loads(line)
+                rows.append(data)
     except:
-        print(i)
-        # raise
-print(lines)
+        print(f"page {i} failed")
+
+with open('innovators.csv', 'w', newline='') as csv_f:
+    writer = csv.writer(csv_f)
+    writer.writerows(rows)
+# print(lines)
 
 
