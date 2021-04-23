6c687dd3081baaf47441b55f1170c6080776c924
Stefan Vercillo <svercillo7@gmail.com>
2021-04-23 17:10:02 -0400

    csv file uploaded

--- Data/concat_to_csv.py
+++ Data/concat_to_csv.py
@@ -25,20 +25,41 @@ import csv
 lines = "" 
 
 rows = []
+col_names = []
+first = True
 for i in range(1, 450):
     try:
         with open(f"data{i}.txt", "r", encoding="utf8") as f:
             flines = f.readlines()
             for line in flines:
                 lines += line
+                
                 data = json.loads(line)
-                rows.append(data)
+                rows.append([data[k] for k in data])
+                col_names = [k for k in data] 
+        
+        if len(rows) > 150:
+            with open('a.csv', 'a+', newline='') as csv_f:
+                writer = csv.writer(csv_f)
+                if first:
+                    writer.writerows([col_names])
+                    first = False
+
+                writer.writerows(rows)
+                rows = [] 
     except:
         print(f"page {i} failed")
 
-with open('innovators.csv', 'w', newline='') as csv_f:
+
+with open('a.csv', 'a+', newline='') as csv_f:
     writer = csv.writer(csv_f)
+    if first:
+        writer.writerows(col_names)
+        first = False
+
     writer.writerows(rows)
-# print(lines)
+    rows = [] 
+    
+    
 
 
